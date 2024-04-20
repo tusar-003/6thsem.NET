d958c984a42d645ce2f0ffee6921e7952879f698
Tusar Mondal <tusar4553@gmail.com>
2024-04-20 19:00:31 +0530

    commit

--- Armstrong between range/armstrong/obj/Debug/net8.0/armstrong.AssemblyInfo.cs
+++ Armstrong between range/armstrong/obj/Debug/net8.0/armstrong.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("armstrong")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+974a3949c4c5d1cd44ae40beff7f35e2333db096")]
 [assembly: System.Reflection.AssemblyProductAttribute("armstrong")]
 [assembly: System.Reflection.AssemblyTitleAttribute("armstrong")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
